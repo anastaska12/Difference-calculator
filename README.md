fa9b138054e5588c4a865f80e0174e4ffd1826f9
anastaska12 <abeliaewa@gmail.com>
2023-07-03 16:55:08 +0300

    fix 3 step

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Difference-calculator
 
+[![linter-status](https://github.com/anastaska12/Difference-calculator/actions/workflows/linter-check.yml/badge.svg)](https://github.com/anastaska12/Difference-calculator/actions/workflows/linter-check.yml)
 [![Maintainability](https://api.codeclimate.com/v1/badges/ad293a7c785e07833100/maintainability)](https://codeclimate.com/github/anastaska12/Difference-calculator/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/ad293a7c785e07833100/test_coverage)](https://codeclimate.com/github/anastaska12/Difference-calculator/test_coverage)
 
--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
-import { readFileSync } from "node:fs";
-import _ from "lodash";
-import path from "path";
+/* eslint-disable no-restricted-syntax */
+import _ from 'lodash';
+import { readFileSync } from 'fs';
+import path from 'path';
 
-const resolve = (fileName) => path.resolve("__fixtures__", fileName);
+const buildFixturesPath = (fileName) => path.resolve('__fixtures__', fileName);
 
 const genDiff = (filepath1, filepath2) => {
-  const data1 = readFileSync(resolve(filepath1), "utf-8");
-  const data2 = readFileSync(resolve(filepath2), "utf-8");
+  const data1 = readFileSync(buildFixturesPath(filepath1), 'utf-8');
+  const data2 = readFileSync(buildFixturesPath(filepath2), 'utf-8');
 
   const dataParse1 = JSON.parse(data1);
   const dataParse2 = JSON.parse(data2);
 
-  const keys1 = _.keys(dataParse1);
-  const keys2 = _.keys(dataParse2);
-  const allKeys = [...keys1, ...keys2];
+  const key1 = _.keys(dataParse1);
+  const key2 = _.keys(dataParse2);
+  const allKeys = _.union(key1, key2);
 
   const diff = {};
-
+  
   for (const key of allKeys) {
     switch (true) {
-      case !keys1.includes(key):
+      case !key1.includes(key):
         diff[`+ ${key}`] = dataParse2[key];
         break;
-      case !keys2.includes(key):
+      case !key2.includes(key):
         diff[`- ${key}`] = dataParse1[key];
         break;
       case dataParse1[key] !== dataParse2[key]:
         diff[`- ${key}`] = dataParse1[key];
         diff[`+ ${key}`] = dataParse2[key];
         break;
+      case dataParse1[key] === dataParse2[key]:
+        diff[`  ${key}`] = dataParse1[key];
+        break;
       default:
         break;
     }
   }
-
   return diff;
 };
 export default genDiff;
